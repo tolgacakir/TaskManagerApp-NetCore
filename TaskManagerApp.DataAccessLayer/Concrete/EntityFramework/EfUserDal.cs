b9a01993151a206c9171ad05f04026bc51f6ccec
Tolga Çakır <cakrtolga13@gmail.com>
2020-08-26 20:41:50 +0300

    Entity specific repositories were implemented for EF Core

---
+++ TaskManagerApp.DataAccessLayer/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagerApp.Core.DataAccess.EntityFramework;
+using TaskManagerApp.DataAccessLayer.Abstract;
+using TaskManagerApp.Entities.Concrete;
+
+namespace TaskManagerApp.DataAccessLayer.Concrete.EntityFramework
+{
+    public class EfUserDal :EfEntityRepositoryBase<User,TaskManagerDbContext>, IUserDal
+    {
+    }
+}
