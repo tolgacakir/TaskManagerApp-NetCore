ac55ca2543f0733269f25027324b87357f79c0ed
Tolga Çakır <cakrtolga13@gmail.com>
2020-08-26 19:51:39 +0300

    The generic repository interfaces were added to Core.DataAccess

---
+++ TaskManagerApp.Core/DataAccess/IQueryableRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskManagerApp.Core.Entities;
+
+namespace TaskManagerApp.Core.DataAccess
+{
+    public interface IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        IQueryable<T> Table { get; }
+    }
+}
