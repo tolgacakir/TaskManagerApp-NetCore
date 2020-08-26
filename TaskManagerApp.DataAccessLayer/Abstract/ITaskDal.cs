390a1aea1618e508e346e2296df817e5b9a5e2a7
Tolga Çakır <cakrtolga13@gmail.com>
2020-08-26 20:08:13 +0300

    Entity specific repository interfaces were created in data access layer.

---
+++ TaskManagerApp.DataAccessLayer/Abstract/ITaskDal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagerApp.Core.DataAccess;
+using TaskManagerApp.Entities.Concrete;
+
+namespace TaskManagerApp.DataAccessLayer.Abstract
+{
+    public interface ITaskDal : IEntityRepository<Task>
+    {
+    }
+}
---
+++ TaskManagerApp.DataAccessLayer/Abstract/IUserDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagerApp.Core.DataAccess;
+using TaskManagerApp.Core.Entities;
+using TaskManagerApp.Entities.Concrete;
+
+namespace TaskManagerApp.DataAccessLayer.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+    }
+}
