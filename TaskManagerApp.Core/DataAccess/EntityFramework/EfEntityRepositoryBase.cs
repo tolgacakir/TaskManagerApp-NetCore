d9d6754319acfc33dda131b19f3ee49c06d4e210
Tolga Çakır <cakrtolga13@gmail.com>
2020-08-26 20:00:14 +0300

    Generic repositories were implemented for EF Core.

---
+++ TaskManagerApp.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using TaskManagerApp.Core.Entities;
+
+namespace TaskManagerApp.Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public TEntity Add(TEntity entity)
+        {
+            using var context = new TContext();
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            context.SaveChanges();
+            return entity;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using var context = new TContext();
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using var context = new TContext();
+            return context.Set<TEntity>().SingleOrDefault(filter);
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using var context = new TContext();
+            return filter == null
+                ? context.Set<TEntity>().ToList()
+                : context.Set<TEntity>().Where(filter).ToList();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using var context = new TContext();
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Modified;
+            context.SaveChanges();
+            return entity;
+        }
+    }
+}
---
+++ TaskManagerApp.Core/DataAccess/EntityFramework/EfQueryableRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskManagerApp.Core.Entities;
+
+namespace TaskManagerApp.Core.DataAccess.EntityFramework
+{
+    public class EfQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private DbContext _context;
+        private DbSet<T> _entities;
+        public EfQueryableRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> Table => this.Entities;
+
+        protected virtual DbSet<T> Entities => _entities ?? (_entities = _context.Set<T>());
+    }
+}
