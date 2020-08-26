0902184ebf27d88fd49c76c003ae6a38d8394739
Tolga Çakır <cakrtolga13@gmail.com>
2020-08-26 19:38:08 +0300

    Entities were implemented.

---
+++ TaskManagerApp.Entities/Concrete/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagerApp.Entities.Concrete
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte TypeId { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime StartingDate { get; set; }
+    }
+}
---
+++ TaskManagerApp.Entities/Concrete/TaskType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagerApp.Entities.Concrete
+{
+    public class TaskType
+    {
+        public byte Id { get; set; }
+        public string Description { get; set; }
+    }
+}
